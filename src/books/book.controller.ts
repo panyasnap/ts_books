b6762966c1ff8ec1ff0f3c9cd4932e97ccb65409
p.antonova <P2202psnap>
2022-11-23 18:46:34 +0300

    mongo

--- src/books/book.controller.ts
+++ src/books/book.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Delete, Get, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Get, Param, Post, Put} from "@nestjs/common";
 import {BookService} from "./book.service";
 import {BookItem} from "../data/book.item";
 import {Book} from "../interfaces/book.interface";
@@ -10,23 +10,23 @@ export class BookController {
     constructor(private readonly bookService: BookService) {
     }
     @Post()
-    create(@Body() book: Book) {
+   public create(@Body() book: Book) {
         this.bookService.create(book);
     }
-    @Post("update")
-    update(@Body()id: string, book: Book) {
+    @Put(":id")
+    public update(@Param() {id}:Book, @Body() book: Book) {
         this.bookService.update(id, book);
     }
     @Get()
-    findAll():Promise<BookDocument[]>{
+    public findAll():Promise<BookDocument[]>{
         return this.bookService.findAll();
     }
    @Get("id")
-    findOne(id: string): QueryWithHelpers<HydratedDocument<BookDocument, {}, {}> | null, HydratedDocument<BookDocument, {}, {}>, {}, BookDocument> {
+   public findOne(id: string): QueryWithHelpers<HydratedDocument<BookDocument, {}, {}> | null, HydratedDocument<BookDocument, {}, {}>, {}, BookDocument> {
        return this.bookService.findOne(id);
    }
    @Delete("id")
-    delete(id: string): QueryWithHelpers<HydratedDocument<BookDocument, {}, {}> | null, HydratedDocument<BookDocument, {}, {}>, {}, BookDocument>{
+   public delete(@Param() {id}:Book): QueryWithHelpers<HydratedDocument<BookDocument, {}, {}> | null, HydratedDocument<BookDocument, {}, {}>, {}, BookDocument>{
         return this.bookService.delete(id)
    }
 }
